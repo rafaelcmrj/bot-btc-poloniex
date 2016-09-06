94848bac3ce9d5740d98873254b5a4dd63139c87
Rafael Costa <rafaelcmrj@gmail.com>
2016-09-06 13:37:31 -0600

    reduced suggested margin to buy to 4%

--- params.sample.js
+++ params.sample.js
@@ -1,7 +1,7 @@
 var params = {
 
     MARGIN_TO_SELL: 2, // margin to sell when currency increases its value (example: sell when currency increases 2.5% of paid value)
-    MARGIN_TO_BUY: 5, // margin to buy when currency decreases its value (example: buy when currency decreases 5% of last order)
+    MARGIN_TO_BUY: 4, // margin to buy when currency decreases its value (example: buy when currency decreases 5% of last order)
     SECURITY_MARGIN: 10 // sell all balance if currency decreases x% after you bought it
 
 };
